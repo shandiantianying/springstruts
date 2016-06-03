e94f7b938d1b36a7090acba3ec48a6ea7803393b
shandiantianying <button.zhou@gamil.com>
2016-06-03 22:03:03 +0800

    添加userList列表
    加userList列表

--- src/cn/shandian/tianying/User/AddUserDao.java
+++ src/cn/shandian/tianying/User/AddUserDao.java
@@ -1,5 +1,7 @@
 package cn.shandian.tianying.User;
 
+import java.util.List;
+
 import org.springframework.transaction.support.TransactionTemplate;
 
 public class AddUserDao {
@@ -12,17 +14,18 @@ public class AddUserDao {
 	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
 		this.transactionTemplate = transactionTemplate;
 	}
-	
-	public void addUser()
-	{
-		
-		
-	///	transactionTemplate.execute(arg0)
-		//transactionTemplate.execute(arg0)
+
+	public void addUser() {
+
+		/// transactionTemplate.execute(arg0)
+		// transactionTemplate.execute(arg0)
 	}
-	
-	public void delUser()
-	{
-		
+
+	public void delUser() {
+
+	}
+
+	public List<User> getlistuser() {
+		return null;
 	}
 }
